21c18ac0c1b481e1a044119991afdc817ddb6b7f
Pedro Rafael Madeira Vasconcelos <pedromadeira13@outlook.com>
2021-07-28 16:43:19 -0300

    changing indentation

--- primeiroProjetoKotlin/api-collections/src/main/kotlin/one/digitalinnovation/collections/teste/TesteFuncionarioGerente.kt
+++ primeiroProjetoKotlin/api-collections/src/main/kotlin/one/digitalinnovation/collections/teste/TesteFuncionarioGerente.kt
@@ -1,12 +1,11 @@
 package one.digitalinnovation.collections.teste
 
-import one.digitalinnovation.collections.Analista
-import one.digitalinnovation.collections.Funcionario
 import one.digitalinnovation.collections.Gerente
-import one.digitalinnovation.collections.Pessoa
-import java.math.BigDecimal
+
 
 fun main() {
-    val pedro = Gerente("Pedro Madeira", "123.879.657-90", 5000.0)
+    val pedro = Gerente("Pedro Madeira", "123.879.657-90", 5000.0, "senha123")
     ImprimeRelatorioFuncionario.imprime(pedro)
+
+    TesteAutenticacao().autentica(pedro)
 }
